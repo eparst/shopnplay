e1466a6384dcbe08d9482ca8b95fc809b9f164f5
saurabh sharma <saurshaz@saurabhs-MacBook-Pro.local>
2013-08-27 13:17:20 +0530

    added new tests, fixed tests workaround for 2.1.3 bug

--- test/com/saurshaz/poc/play/ApplicationTest.java
+++ test/com/saurshaz/poc/play/ApplicationTest.java
@@ -1,22 +1,12 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+package com.saurshaz.poc.play;
 
-import org.codehaus.jackson.JsonNode;
-import org.junit.*;
+import static org.fest.assertions.Assertions.assertThat;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
 
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
+import org.junit.Test;
 
-import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
+import play.mvc.Content;
 
 
 /**
